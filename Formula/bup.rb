e0d7a4f6b4daa2a6f68267d151b5b588dbd467e0
Paul Meinhardt <mnhrdt@gmail.com>
2021-04-07 23:21:04 +0200

    Switch to tar-bundles + checksum installs

--- Formula/bup.rb
+++ Formula/bup.rb
@@ -4,10 +4,8 @@ class Bup < Formula
   desc "Interactively upgrade installed brew formulae and casks"
   homepage "https://github.com/pmeinhardt/homebrew-tools"
   head "https://github.com/pmeinhardt/homebrew-tools.git", branch: "main"
-  url "https://github.com/pmeinhardt/homebrew-tools.git",
-      using: :git,
-      branch: "main",
-      revision: "1a10359f04a76302d37b8d00c026358f824fbfad"
+  url "https://github.com/pmeinhardt/homebrew-tools/archive/1a10359f04a76302d37b8d00c026358f824fbfad.tar.gz"
+  sha256 "2a44eed624d0134f4eb98e446de576b3fb8e59ec0c458acf56439b5560102e54"
   version "0.1.0"
 
   bottle :unneeded
