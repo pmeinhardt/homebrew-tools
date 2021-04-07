335ec6ae6a000537130a7a427a82e8dcfd043ce8
Paul Meinhardt <mnhrdt@gmail.com>
2021-04-07 22:50:48 +0200

    Specify current stable versions for formulae

--- Formula/bup.rb
+++ Formula/bup.rb
@@ -4,6 +4,11 @@ class Bup < Formula
   desc "Interactively upgrade installed brew formulae and casks"
   homepage "https://github.com/pmeinhardt/homebrew-tools"
   head "https://github.com/pmeinhardt/homebrew-tools.git", branch: "main"
+  url "https://github.com/pmeinhardt/homebrew-tools.git",
+      using: :git,
+      branch: "main",
+      revision: "1a10359f04a76302d37b8d00c026358f824fbfad"
+  version "0.1.0"
 
   bottle :unneeded
 
--- Formula/hmm.rb
+++ Formula/hmm.rb
@@ -4,6 +4,11 @@ class Hmm < Formula
   desc "Find out what you did today"
   homepage "https://github.com/pmeinhardt/hmm"
   head "https://github.com/pmeinhardt/hmm.git", branch: "main"
+  url "https://github.com/pmeinhardt/hmm.git",
+      using: :git,
+      branch: "main",
+      revision: "072d7b6f4ca4e49efb9e6cfc296efb0b6b2d3629"
+  version "1.0.0"
 
   bottle :unneeded
 
