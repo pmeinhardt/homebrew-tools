5d7692e695920e90739b3bdee122c1b2bb526c34
Paul Meinhardt <mnhrdt@gmail.com>
2021-04-07 23:21:04 +0200

    Switch to tar-bundles + checksum installs

--- Formula/hmm.rb
+++ Formula/hmm.rb
@@ -4,10 +4,8 @@ class Hmm < Formula
   desc "Find out what you did today"
   homepage "https://github.com/pmeinhardt/hmm"
   head "https://github.com/pmeinhardt/hmm.git", branch: "main"
-  url "https://github.com/pmeinhardt/hmm.git",
-      using: :git,
-      branch: "main",
-      revision: "072d7b6f4ca4e49efb9e6cfc296efb0b6b2d3629"
+  url "https://github.com/pmeinhardt/hmm/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "e29732283729554dce09c7d5a30170df24f25a7cef503937edfcd7c05b1f3cd7"
   version "1.0.0"
 
   bottle :unneeded
