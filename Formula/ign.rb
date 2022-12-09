7c15f92891e260ef0d36919da3d1d9d2d03ea23a
pmeinhardt <pmeinhardt@users.noreply.github.com>
2022-12-09 19:30:52 +0000

    Update formulae
    - ign 0.2.0

--- Formula/ign.rb
+++ Formula/ign.rb
@@ -4,9 +4,9 @@ class Ign < Formula
   desc "Get .gitignore templates from GitHub"
   homepage "https://github.com/pmeinhardt/homebrew-tools"
   head "https://github.com/pmeinhardt/homebrew-tools.git", branch: "main"
-  url ""
-  sha256 ""
-  version "0.1.0"
+  url "https://github.com/pmeinhardt/homebrew-tools/archive/0e4e86fef08ad66f690190bb34fdde6196c9f7cb.tar.gz"
+  sha256 "fed3dd5bf6f00dd9cf72a6b7ce616806509a1c30984ac66daba379fc735baa4c"
+  version "0.2.0"
 
   depends_on "fzf"
   depends_on "gh"
