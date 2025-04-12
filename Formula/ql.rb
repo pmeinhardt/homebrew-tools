ba77baac91197d5e204093c4e1a778980063c68e
pmeinhardt <code@mnhrdt.io>
2025-04-12 18:46:28 +0200

    Add formula to install pmeinhardt/ql

---
+++ Formula/ql.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Ql < Formula
+  desc "Quick Look from the command-line"
+  homepage "https://github.com/pmeinhardt/ql"
+  head "git@github.com:pmeinhardt/ql.git", branch: "main"
+  license "MIT"
+
+  depends_on :macos
+
+  uses_from_macos "swift" => :build
+
+  def install
+    system "swift", "build", "--configuration", "release"
+    bin.install ".build/release/ql"
+  end
+end
